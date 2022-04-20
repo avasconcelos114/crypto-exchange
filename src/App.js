f36219e585d33393794c6d2139f606aaae9fd4e1
Andre Vasconcelos <andre.onogoro@gmail.com>
2022-04-20 23:33:55 +0900

    Added cards to main App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import BinanceCard from '~components/BinanceCard';
+import BitfinexCard from '~components/BitfinexCard';
+import HuobiCard from '~components/HuobiCard';
+import KrakenCard from '~components/KrakenCard';
 
 function App() {
-  return <div className="App"></div>;
+  const [pair, setPair] = useState('BTC/USDT');
+
+  return (
+    <div className="App">
+      <h2>{pair}</h2>
+      <BinanceCard pair={pair} />
+      <BitfinexCard pair={pair} />
+      <HuobiCard pair={pair} />
+      <KrakenCard pair={pair} />
+    </div>
+  );
 }
 
 export default App;
